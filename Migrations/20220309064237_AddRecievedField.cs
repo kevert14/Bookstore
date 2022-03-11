9a42b9450176819160d0c697d46abe81438324e2
Kyle Evert <kevert14@byu.edu>
2022-03-10 22:15:46 -0700

    Mission 10

---
+++ Migrations/20220309064237_AddRecievedField.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bookstore.Migrations
+{
+    public partial class AddRecievedField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "PurchaseRecieved",
+                table: "Purchases",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PurchaseRecieved",
+                table: "Purchases");
+        }
+    }
+}
--- Models/Purchase.cs
+++ Models/Purchase.cs
@@ -35,5 +35,8 @@ namespace Bookstore.Models
         public string Country { get; set; }
 
         public bool Anonymous { get; set; }
+
+        [BindNever]
+        public bool PurchaseRecieved { get; set; }
     }
 }
